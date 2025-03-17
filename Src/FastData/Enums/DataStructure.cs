9c32a246f1c07b154c4761184106b85e8a8b02b6
Ian Qvist <qvistian@gmail.com>
2025-03-17 13:11:33 +0100

    Add a few notes to DataStructure about data complexity

--- Src/FastData/Enums/DataStructure.cs
+++ Src/FastData/Enums/DataStructure.cs
@@ -2,16 +2,25 @@ namespace Genbox.FastData.Enums;
 
 public enum DataStructure : byte
 {
+    // We always need something to represent a single value
+    SingleValue,
+
+    // O(n) data structures
     Array,
+    Conditional,
+    Switch,
+
+    // O(log(n)) data structures
     BinarySearch,
     EytzingerSearch,
-    Switch,
+
+    // O(1) data structures
     MinimalPerfectHash,
     HashSetLinear,
     HashSetChain,
+
+    // Edge-cases
+    KeyLength,
     UniqueKeyLength,
     UniqueKeyLengthSwitch,
-    KeyLength,
-    SingleValue,
-    Conditional
 }
