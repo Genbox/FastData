18b127e8878e6e90914e6cbebd686efbb792f536
Ian Qvist <qvistian@gmail.com>
2025-04-21 16:43:57 +0200

    Remove unused extension

--- Src/FastData/Extensions/DataTypeExtensions.cs
+++ Src/FastData/Extensions/DataTypeExtensions.cs
@@ -4,13 +4,6 @@ namespace Genbox.FastData.Extensions;
 
 public static class DataTypeExtensions
 {
-    public static bool IsUnsigned(this DataType type) => type switch
-    {
-        DataType.SByte or DataType.Int16 or DataType.Int32 or DataType.Int64 or DataType.Single or DataType.Double => false,
-        DataType.UInt32 or DataType.UInt16 or DataType.UInt64 or DataType.Byte or DataType.Char => true,
-        _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-    };
-
     public static bool IsInteger(this DataType type) => type switch
     {
         DataType.SByte or DataType.Int16 or DataType.Int32 or DataType.Int64 or DataType.Single or DataType.Double or DataType.UInt32 or DataType.UInt16 or DataType.UInt64 or DataType.Byte or DataType.Char => true,
