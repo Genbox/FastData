eeaf432f977e279ad61b5134b5a42f7b8fc35710
Ian Qvist <qvistian@gmail.com>
2025-04-25 19:24:42 +0200

    Add custom GetHashCode for double/float + benchmark

---
+++ Src/FastData.Benchmarks/Benchmarks/GetHashCodeBenchmarks.cs
@@ -0,0 +1,41 @@
+using System.Runtime.CompilerServices;
+
+namespace Genbox.FastData.Benchmarks.Benchmarks;
+
+public class GetHashCodeBenchmarks
+{
+    private readonly double _value = 12.5; //Don't make const (it will make the compiler optimize further). We need to simulate real-lfie.
+
+    [Benchmark]
+    public uint DoubleHashCode()
+    {
+        uint val = 0;
+
+        for (int i = 0; i < 1000; i++)
+            val += (uint)_value.GetHashCode();
+
+        return val;
+    }
+
+    [Benchmark]
+    public uint DoubleHashCodeCustom()
+    {
+        uint val = 0;
+
+        for (int i = 0; i < 1000; i++)
+            val += GetHashCode(_value);
+
+        return val;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static uint GetHashCode(double value)
+    {
+        ulong bits = Unsafe.ReadUnaligned<ulong>(ref Unsafe.As<double, byte>(ref value));
+
+        if (((bits - 1) & ~(0x8000_0000_0000_0000)) >= 0x7FF0_0000_0000_0000)
+            bits &= 0x7FF0_0000_0000_0000;
+
+        return unchecked((uint)bits) ^ (uint)(bits >> 32);
+    }
+}
--- Src/FastData/Specs/Hash/DefaultHashSpec.cs
+++ Src/FastData/Specs/Hash/DefaultHashSpec.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Genbox.FastData.Abstracts;
 using Genbox.FastData.Internal.Hashes;
 
@@ -22,11 +23,33 @@ public sealed class DefaultHashSpec : IHashSpec
             ushort val => val,
             int val => (uint)val,
             uint val => val,
+            float val => GetHashCode(val),
+            double val => GetHashCode(val),
             _ => (uint)obj.GetHashCode()
         };
 
         return hash;
     };
+
+    private static uint GetHashCode(float value)
+    {
+        uint bits = Unsafe.ReadUnaligned<uint>(ref Unsafe.As<float, byte>(ref value));
+
+        if (((bits - 1) & ~(0x8000_0000)) >= 0x7F80_0000)
+            bits &= 0x7F80_0000;
+
+        return bits;
+    }
+
+    private static uint GetHashCode(double value)
+    {
+        ulong bits = Unsafe.ReadUnaligned<ulong>(ref Unsafe.As<double, byte>(ref value));
+
+        if (((bits - 1) & ~(0x8000_0000_0000_0000)) >= 0x7FF0_0000_0000_0000)
+            bits &= 0x7FF0_0000_0000_0000;
+
+        return unchecked((uint)bits) ^ (uint)(bits >> 32);
+    }
 
     public EqualFunc GetEqualFunction() => static (a, b) => ((string)a).Equals((string)b, StringComparison.Ordinal);
 }
