33a9d0013bbee47d4749e6b32f28e84c681d4209
Ian Qvist <qvistian@gmail.com>
2025-05-03 20:37:05 +0200

    Ensure the input to get_hash is const

--- Src/FastData.Generator.CPlusPlus/Internal/Extensions/GeneratorConfigExtensions.cs
+++ Src/FastData.Generator.CPlusPlus/Internal/Extensions/GeneratorConfigExtensions.cs
@@ -22,13 +22,19 @@ internal static class GeneratorConfigExtensions
         _ => throw new InvalidOperationException("Invalid DataType: " + config.DataType)
     };
 
-    internal static string GetHashSource(this GeneratorConfig config)
+    internal static string GetHashSource(this GeneratorConfig config) =>
+        $$"""
+              static uint32_t get_hash(const {{config.GetTypeName()}} value)
+              {
+          {{GetHash(config.DataType)}}
+              }
+          """;
+
+    private static string GetHash(DataType dataType)
     {
-        if (config.DataType == DataType.String)
+        if (dataType == DataType.String)
         {
             return """
-                       static uint32_t get_hash(const std::string_view value)
-                       {
                            uint32_t hash = 352654597;
 
                            const char* ptr = value.data();
@@ -40,58 +46,37 @@ internal static class GeneratorConfigExtensions
                            }
 
                            return 352654597 + (hash * 1566083941);
-                       }
                    """;
         }
 
-        if (config.DataType.IsIdentityHash() || config.DataType == DataType.Boolean)
-        {
-            return $$"""
-                      static uint32_t get_hash({{config.GetTypeName()}} value)
-                      {
-                          return static_cast<uint32_t>(value);
-                      }
-                      """;
-        }
+        if (dataType.IsIdentityHash() || dataType == DataType.Boolean)
+            return "        return static_cast<uint32_t>(value);";
 
-        if (config.DataType is DataType.Int64 or DataType.UInt64)
+        if (dataType is DataType.Int64 or DataType.UInt64)
+            return "        return static_cast<uint32_t>(static_cast<int32_t>(value) ^ static_cast<int32_t>(value >> 32));";
+
+        if (dataType == DataType.Single)
         {
-            return $$"""
-                   static uint32_t get_hash({{config.GetTypeName()}} value)
-                   {
-                       return static_cast<uint32_t>(static_cast<int32_t>(value) ^ static_cast<int32_t>(value >> 32));
-                   }
+            return """
+                           uint32_t bits;
+                           std::memcpy(&bits, &value, sizeof(bits));
+                           if (((bits - 1) & ~0x80000000u) >= 0x7F800000u)
+                               bits &= 0x7F800000u;
+                           return bits;
                    """;
         }
 
-        if (config.DataType == DataType.Single)
+        if (dataType == DataType.Double)
         {
-            return $$"""
-                          static uint32_t get_hash({{config.GetTypeName()}} value)
-                          {
-                              uint32_t bits;
-                              std::memcpy(&bits, &value, sizeof(bits));
-                              if (((bits - 1) & ~0x80000000u) >= 0x7F800000u)
-                                  bits &= 0x7F800000u;
-                              return bits;
-                          }
-                      """;
+            return """
+                           uint64_t bits;
+                           std::memcpy(&bits, &value, sizeof(bits));
+                           if (((bits - 1) & ~0x8000000000000000ull) >= 0x7FF0000000000000ull)
+                               bits &= 0x7FF0000000000000ull;
+                           return static_cast<uint32_t>(bits) ^ static_cast<uint32_t>(bits >> 32);
+                   """;
         }
 
-        if (config.DataType == DataType.Double)
-        {
-            return $$"""
-                          static uint32_t get_hash({{config.GetTypeName()}} value)
-                          {
-                              uint64_t bits;
-                              std::memcpy(&bits, &value, sizeof(bits));
-                              if (((bits - 1) & ~0x8000000000000000ull) >= 0x7FF0000000000000ull)
-                                  bits &= 0x7FF0000000000000ull;
-                              return static_cast<uint32_t>(bits) ^ static_cast<uint32_t>(bits >> 32);
-                          }
-                      """;
-        }
-
-        throw new InvalidOperationException("Unsupported data type: " + config.DataType);
+        throw new InvalidOperationException("Unsupported data type: " + dataType);
     }
 }
