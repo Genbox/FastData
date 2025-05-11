8cd2f95c6b1169061e0558ec7d0af5ea2c66e1ae
Ian Qvist <qvistian@gmail.com>
2025-05-11 15:20:57 +0200

    Fix bug introduced with templates

--- Src/FastData.Generator.CPlusPlus/Internal/Framework/CPlusPlusConstantsDef.cs
+++ Src/FastData.Generator.CPlusPlus/Internal/Framework/CPlusPlusConstantsDef.cs
@@ -5,9 +5,9 @@ namespace Genbox.FastData.Generator.CPlusPlus.Internal.Framework;
 internal class CPlusPlusConstantsDef : IConstantsDef
 {
     public string Comment => "//";
-    public Func<string, string, string> MinLengthTemplate => (type, value) => $"static constexpr {type} min_length = {value}";
-    public Func<string, string, string> MaxLengthTemplate => (type, value) => $"static constexpr {type} max_length = {value}";
-    public Func<string, string, string> MinValueTemplate => (type, value) => $"static constexpr {type} min_value = {value}";
-    public Func<string, string, string> MaxValueTemplate => (type, value) => $"static constexpr {type} max_value = {value}";
-    public Func<string, string, string> ItemCountTemplate => (type, value) => $"static constexpr {type} item_count = {value}";
+    public Func<string, string, string> MinLengthTemplate => (type, value) => $"    static constexpr {type} min_length = {value};";
+    public Func<string, string, string> MaxLengthTemplate => (type, value) => $"    static constexpr {type} max_length = {value};";
+    public Func<string, string, string> MinValueTemplate => (type, value) => $"    static constexpr {type} min_value = {value};";
+    public Func<string, string, string> MaxValueTemplate => (type, value) => $"    static constexpr {type} max_value = {value};";
+    public Func<string, string, string> ItemCountTemplate => (type, value) => $"    static constexpr {type} item_count = {value};";
 }
--- Src/FastData.Generator.CSharp/Internal/Framework/CSharpConstantsDef.cs
+++ Src/FastData.Generator.CSharp/Internal/Framework/CSharpConstantsDef.cs
@@ -5,9 +5,9 @@ namespace Genbox.FastData.Generator.CSharp.Internal.Framework;
 internal class CSharpConstantsDef : IConstantsDef
 {
     public string Comment => "//";
-    public Func<string, string, string> MinLengthTemplate => (type, value) => $"public const {type} MinLength = {value}";
-    public Func<string, string, string> MaxLengthTemplate => (type, value) => $"public const {type} MaxLength = {value}";
-    public Func<string, string, string> MinValueTemplate => (type, value) => $"public const {type} MinValue = {value}";
-    public Func<string, string, string> MaxValueTemplate => (type, value) => $"public const {type} MaxValue = {value}";
-    public Func<string, string, string> ItemCountTemplate => (type, value) => $"public const {type} ItemCount = {value}";
+    public Func<string, string, string> MinLengthTemplate => (type, value) => $"    public const {type} MinLength = {value};";
+    public Func<string, string, string> MaxLengthTemplate => (type, value) => $"    public const {type} MaxLength = {value};";
+    public Func<string, string, string> MinValueTemplate => (type, value) => $"    public const {type} MinValue = {value};";
+    public Func<string, string, string> MaxValueTemplate => (type, value) => $"    public const {type} MaxValue = {value};";
+    public Func<string, string, string> ItemCountTemplate => (type, value) => $"    public const {type} ItemCount = {value};";
 }
