7ef174896e3bde8f79f8777e83b787ecf22fba7e
Ian Qvist <qvistian@gmail.com>
2025-05-11 16:06:01 +0200

    Add benchmark for 64bit hashcodes

--- Src/FastData.Benchmarks/Benchmarks/GetHashCodeBenchmarks.cs
+++ Src/FastData.Benchmarks/Benchmarks/GetHashCodeBenchmarks.cs
@@ -28,6 +28,17 @@ public class GetHashCodeBenchmarks
         return val;
     }
 
+    [Benchmark]
+    public ulong DoubleHashCodeCustom64()
+    {
+        ulong val = 0;
+
+        for (int i = 0; i < 1000; i++)
+            val += GetHashCode64(_value);
+
+        return val;
+    }
+
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static uint GetHashCode(double value)
     {
@@ -37,5 +48,16 @@ public class GetHashCodeBenchmarks
             bits &= 0x7FF0_0000_0000_0000;
 
         return unchecked((uint)bits) ^ (uint)(bits >> 32);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static ulong GetHashCode64(double value)
+    {
+        ulong bits = Unsafe.ReadUnaligned<ulong>(ref Unsafe.As<double, byte>(ref value));
+
+        if (((bits - 1) & ~(0x8000_0000_0000_0000)) >= 0x7FF0_0000_0000_0000)
+            bits &= 0x7FF0_0000_0000_0000;
+
+        return bits;
     }
 }
