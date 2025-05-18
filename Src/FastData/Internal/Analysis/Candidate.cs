57011c62a4cb761f82a175fd3556ff61ce862170
Ian Qvist <qvistian@gmail.com>
2025-05-18 19:54:25 +0200

    We don't need equality on candidates

--- Src/FastData/Internal/Analysis/Candidate.cs
+++ Src/FastData/Internal/Analysis/Candidate.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using Genbox.FastData.Abstracts;
 
 namespace Genbox.FastData.Internal.Analysis;
@@ -11,10 +10,4 @@ internal sealed class Candidate<T> where T : IStringHash
 
     public Candidate(T spec) => Spec = spec;
     public Candidate() { }
-
-    public override bool Equals(object? obj) => obj is Candidate<T> other && Equals(other);
-    public bool Equals(Candidate<T> other) => Spec.Equals(other.Spec) && Fitness.Equals(other.Fitness);
-
-    [SuppressMessage("Correctness", "SS008:GetHashCode() refers to mutable or static member")]
-    public override int GetHashCode() => Spec.GetHashCode() ^ Fitness.GetHashCode();
 }
