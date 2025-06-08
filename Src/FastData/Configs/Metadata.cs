e215d8c65b39fbeb9c4569e07c4488e2f39989ad
Ian Qvist <qvistian@gmail.com>
2025-06-08 16:37:42 +0200

    Update packages

--- Src/FastData/Configs/Metadata.cs
+++
@@ -1,9 +0,0 @@
-using System.Globalization;
-
-namespace Genbox.FastData.Configs;
-
-public sealed class Metadata(Version version, DateTimeOffset timestamp)
-{
-    public string Program { get; } = "FastData " + version;
-    public string Timestamp { get; } = timestamp.ToString("yyyy-MM-dd HH:mm:ss", DateTimeFormatInfo.InvariantInfo) + " UTC";
-}
--- Src/FastData/Configs/SimulatorConfig.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.Annotations;
-
-namespace Genbox.FastData.Configs;
-
-[UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
-public sealed class SimulatorConfig
-{
-    public double CapacityFactor { get; set; } = 1.0;
-}
