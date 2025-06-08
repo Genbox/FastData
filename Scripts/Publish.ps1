3c652621e8b2d3168a439197948cbcbc947e4cf9
Ian Qvist <qvistian@gmail.com>
2025-06-08 20:22:47 +0200

    Fixes to publish script

--- Scripts/Publish.ps1
+++ Scripts/Publish.ps1
@@ -4,8 +4,18 @@ $PublishDir = "$Root/Publish"
 $ArtifactsDir = "$Root/Publish/Artifacts"
 $Color = "Blue"
 
+# Prerequsites
+Write-Host -ForegroundColor $Color "Installing prerequisites"
+dotnet tool install --global minver-cli
+
 Write-Host -ForegroundColor $Color "Clean up from previous publishes"
 Remove-Item -Path $PublishDir/* -Recurse -Force -ErrorAction Ignore | Out-Null
+
+$version = minver;
+Write-Host -ForegroundColor Cyan "Version: $version"
+
+# Override version so minver does not calculate it per package
+$env:MinVerVersionOverride = $version
 
 Write-Host -ForegroundColor $Color "Publish the dll files"
 $PwshFramework = "netstandard2.0" # Version needed by PowerShell
@@ -14,12 +24,6 @@ dotnet publish $Root/Src/FastData.Generator/FastData.Generator.csproj -c $Config
 dotnet publish $Root/Src/FastData.Generator.CSharp/FastData.Generator.CSharp.csproj -c $Config -f $PwshFramework -p:GenerateDependencyFile=false -o $ArtifactsDir
 dotnet publish $Root/Src/FastData.Generator.CPlusPlus/FastData.Generator.CPlusPlus.csproj -c $Config -f $PwshFramework -p:GenerateDependencyFile=false -o $ArtifactsDir
 dotnet publish $Root/Src/FastData.Generator.Rust/FastData.Generator.Rust.csproj -c $Config -f $PwshFramework -p:GenerateDependencyFile=false -o $ArtifactsDir
-
-Write-Host -ForegroundColor $Color "Get the version number of the generator dll file"
-$ver = [System.Reflection.AssemblyName]::GetAssemblyName("$ArtifactsDir/Genbox.FastData.dll").Version
-$semver = "$($ver.Major).$($ver.Minor).$($ver.Build)"
-
-Write-Host -ForegroundColor Cyan "Version: $semver"
 
 Write-Host -ForegroundColor $Color "Pack the CLI tool as executable"
 dotnet publish $Root/Src/FastData.Cli/FastData.Cli.csproj -c $Config -r win-x64 -p:PublishSingleFile=true -p:SelfContained=true -p:PublishTrimmed=true -p:TargetFrameworks="net9.0" -p:DebugType=none -p:GenerateDocumentationFile=false -p:EnableCompressionInSingleFile=true -p:InvariantGlobalization=true -o $PublishDir
@@ -56,7 +60,7 @@ Copy-Item $Root/Misc/PowerShell/FastData.psm1 $PublishDir/Genbox.FastData/Genbox
 Copy-Item $ArtifactsDir/*.dll $PublishDir/Genbox.FastData/lib/
 
 # We don't want to publish versions with tags like "alpha", "beta", etc.
-if ($ver -like "*-*") {
+if ($version -notlike "*-*") {
     if ($env:PWSHG_KEY) {
         Write-Host -ForegroundColor $Color "Publish PowerShell to PowerShell Gallery"
         Publish-Module -Path "$PublishDir/Genbox.FastData/" -NuGetApiKey $env:PWSHG_KEY
@@ -65,7 +69,7 @@ if ($ver -like "*-*") {
         Write-Host -ForegroundColor Yellow "Skipping PowerShell publish: PWSHG_KEY not set."
     }
 
-    if ($env:NUGET_KEY -and ($ver -notlike "*alpha*")) {
+    if ($env:NUGET_KEY) {
         Write-Host -ForegroundColor $Color "Publish dotnet tool to NuGet"
         Get-ChildItem -Path "$PublishDir/*.nupkg" | ForEach-Object {
             dotnet nuget push --skip-duplicate $_.FullName --api-key $env:NUGET_KEY --source https://api.nuget.org/v3/index.json
@@ -74,4 +78,7 @@ if ($ver -like "*-*") {
     else {
         Write-Host -ForegroundColor Yellow "Skipping NuGet push: NUGET_KEY not set."
     }
+}
+else {
+    Write-Host -ForegroundColor Yellow "Skipping publish due to alpha build"
 }
