afaa62f9cdf060c2b2022b6ab5308152c2005c7a
Ian Qvist <qvistian@gmail.com>
2026-05-18 19:25:15 +0200

    Add assertions to GeneratorFunctions to avoid bugs further down in the chain

--- Src/FastData/Generators/GeneratorFunctions.cs
+++ Src/FastData/Generators/GeneratorFunctions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -5,7 +6,12 @@ namespace Genbox.FastData.Generators;
 
 public static class GeneratorFunctions
 {
-    public static uint UnitAt(string str, int offset) => offset >= 0 ? str[offset] : str[str.Length + offset];
+    public static uint UnitAt(string str, int offset)
+    {
+        int index = offset >= 0 ? offset : str.Length + offset;
+        Debug.Assert(IsValidIndex(str, index), "UnitAt requires a non-empty string and a valid offset.");
+        return str[index];
+    }
 
     public static uint UnitAtAsciiLower(string str, int offset)
     {
@@ -19,12 +25,14 @@ public static class GeneratorFunctions
     public static bool EqualsAt(string str, int offset, string fragment)
     {
         int start = offset >= 0 ? offset : str.Length + offset;
+        Debug.Assert(IsValidRange(str, start, fragment.Length), "EqualsAt requires a non-empty string, a valid offset, and a fragment that fits within the string.");
         return string.CompareOrdinal(str, start, fragment, 0, fragment.Length) == 0;
     }
 
     public static bool EqualsAtAsciiLower(string str, int offset, string fragment)
     {
         int start = offset >= 0 ? offset : str.Length + offset;
+        Debug.Assert(IsValidRange(str, start, fragment.Length), "EqualsAtAsciiLower requires a non-empty string, a valid offset, and a fragment that fits within the string.");
 
         for (int i = 0; i < fragment.Length; i++)
         {
@@ -48,4 +56,7 @@ public static class GeneratorFunctions
     public static uint ReadU16(byte[] ptr, int offset) => Unsafe.ReadUnaligned<ushort>(ref Unsafe.Add(ref MemoryMarshal.GetReference(ptr.AsSpan()), offset));
     public static uint ReadU32(byte[] ptr, int offset) => Unsafe.ReadUnaligned<uint>(ref Unsafe.Add(ref MemoryMarshal.GetReference(ptr.AsSpan()), offset));
     public static ulong ReadU64(byte[] ptr, int offset) => Unsafe.ReadUnaligned<ulong>(ref Unsafe.Add(ref MemoryMarshal.GetReference(ptr.AsSpan()), offset));
+
+    private static bool IsValidIndex(string str, int index) => str.Length > 0 && (uint)index < (uint)str.Length;
+    private static bool IsValidRange(string str, int start, int length) => str.Length > 0 && (uint)start <= (uint)str.Length && length <= str.Length - start;
 }
