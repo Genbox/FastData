b35ccd812a7b8add8475a825c3b5d1f6d3b65596
Ian Qvist <qvistian@gmail.com>
2026-06-19 10:51:42 +0200

    Add missing InternalsVisibleTo

--- Src/FastData/Properties/AssemblyInfo.cs
+++ Src/FastData/Properties/AssemblyInfo.cs
@@ -2,3 +2,4 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("FastData.Testbed")]
 [assembly: InternalsVisibleTo("FastData.InternalShared")]
+[assembly: InternalsVisibleTo("FastData.Generator")]
